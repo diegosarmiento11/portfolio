b9b4f0d2e20d7caeac8fcfcc632027f7a15d48cc
Diego Sarmiento <diegosarmientoq1245@gmail.com>
2021-05-10 19:53:10 -0500

    Copywriting modified

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -17,35 +17,35 @@ function Skills(props) {
                 <SkillBadge
                     img={Pencil}
                     title='Visual Design'
-                    p='He desarrollado capacidades autodidactas de aprendizaje y tengo una enorme fascinación por seguir aprendiendo mientras construyo.' 
+                    p='Confident about Figma and Adobe XD for mockups and prototypes' 
                 />
                 <SkillBadge
                     img={Computer}
-                    title='Visual Design'
-                    p='He desarrollado capacidades autodidactas de aprendizaje y tengo una enorme fascinación por seguir aprendiendo mientras construyo. '
+                    title='Marketing'
+                    p='Great skills with Google Analytics, Google Ads and Google Tag Manager'
                 />
                 <SkillBadge
                     img={Code}
-                    title='Visual Design'
-                    p='He desarrollado capacidades autodidactas de aprendizaje y tengo una enorme fascinación por seguir aprendiendo mientras construyo. '
+                    title='Coding'
+                    p='Im mastering React js and improving my JavaScript code, always iterating and learning about my bugs'
                 />
             </div>
             <h3 className='skills_title'>{props.title2}</h3>
             <div className='skills__container'>
                 <SkillBadge
                     img={Communication}
-                    title='Visual Design'
-                    p='He desarrollado capacidades autodidactas de aprendizaje y tengo una enorme fascinación por seguir aprendiendo mientras construyo.'
+                    title='Great communication skills'
+                    p='Because of my studies background i have developed excellent conflict resolution and active listening skill, to me, empathy is a very important value'
                 />
                 <SkillBadge
                     img={Team}
-                    title='Visual Design'
-                    p='He desarrollado capacidades autodidactas de aprendizaje y tengo una enorme fascinación por seguir aprendiendo mientras construyo.'
+                    title='Collaborative'
+                    p='i prioritize group goals over personal recognition or agendas having an open mind about suggestions from team members'
                 />
                 <SkillBadge
                     img={Diego}
-                    title='Visual Design'
-                    p='He desarrollado capacidades autodidactas de aprendizaje y tengo una enorme fascinación por seguir aprendiendo mientras construyo.'
+                    title='Proactivity'
+                    p='I considered myself as very curious, developing self taught abilities and passionate about build bridges instead of walls.'
                 />
             </div>
         </div>
