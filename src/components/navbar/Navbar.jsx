4812e6e9f6e3d896340e0f078fc181e9fb7952b0
Diego Sarmiento <60120140+diegosarmiento11@users.noreply.github.com>
2021-06-01 23:57:15 -0500

    Stack names were added and project width was adjusted usign padding

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,32 +7,32 @@ import DiegoLogo from '../../assets/images/diegoSarmiento.png'
 class Navbar extends Component {
     render(props) {
         return (
-            <div id={this.props.link}>
-                <div className="navbar__container">
-                    <Router>
-                        <Hamburguer/>
-                        <Switch>
-                            <Route path='/'/>
-                        </Switch>
-                    </Router>
-                    <div className="navbar__image--container">
-                        <a href="index.html">
-                            <img src={DiegoLogo} alt="Logo Diegosarmiento.co" />
-                        </a>
-                    </div>
-                    <ul className='navbar__list'>
-                        <li className="list">
-                             <a href="#PROJECTTARGET"> Projects  </a>
-                        </li>
-                        <li className="list">
-                             <a href="#WHOAMITARGET"> Who am i? </a>
-                        </li>
-                        <li className="list">
-                             <a href="#CONTACTMETARGET"> Contact me </a>
-                        </li>
-                    </ul>
-                </div>
+          <div id={this.props.link}>
+            <div className="navbar__container">
+              <Router>
+                <Hamburguer/>
+                <Switch>
+                  <Route path='/'/>
+                </Switch>
+              </Router>
+              <div className="navbar__image--container">
+                <a href="index.html">
+                  <img src={DiegoLogo} alt="Logo Diegosarmiento.co" />
+                </a>
+              </div>
+              <ul className='navbar__list'>
+                <li className="list">
+                  <a href="#PROJECTTARGET"> Projects  </a>
+                </li>
+                <li className="list">
+                  <a href="#WHOAMITARGET"> Who am i? </a>
+                </li>
+                <li className="list">
+                  <a href="#CONTACTMETARGET"> Contact me </a>
+                </li>
+              </ul>
             </div>
+          </div>
         );
     }
 }
